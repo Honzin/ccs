d936acf2c744306d6c6059edca467f365a1e2a73
Honza <xsedaj00@gmail.com>
2017-02-02 19:41:38 +0100

    Add tests

--- setup.py
+++ setup.py
@@ -1,13 +1,30 @@
 from distutils.core import setup
+
+packages = ["ccs",
+            "ccs.bitfinex",
+            "ccs.bitstamp",
+            "ccs.bittrex",
+            "ccs.btcc",
+            "ccs.btccpro",
+            "ccs.btccusd",
+            "ccs.btce",
+            "ccs.bter",
+            "ccs.cexio",
+            "ccs.kraken",
+            "ccs.okcoin",
+            "ccs.okcoincn",
+            "ccs.okcoincom",
+            "ccs.poloniex"
+            ]
 setup(
-  name='ccs',
-  packages=['ccs'],
-  version='0.1',
-  description='Crypto currencies stocks (ccs) is Python package for communication with stocks which are traiding with crypto currencies. ',
-  author='Jan Šeda',
-  author_email='xsedaj00@gmail.com',
-  url='https://github.com/Honzin/ccs', # use the URL to the github repo
-  download_url='https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
-  keywords=['bitfinex', 'bitstamp', 'bittrex', "btcc", "btce", "bter", "cexio", "kraken", "okcoin", "poloniex", "api", "crypto", "coin", "stock", "market"], # arbitrary keywords
-  classifiers=[],
+    name            = 'ccs',
+    packages        = packages,
+    version         = '0.1',
+    description     = 'Crypto currencies stocks (ccs) is Python package for communication with stocks which are traiding with crypto currencies. ',
+    author          = 'Jan Seda',
+    author_email    = 'xsedaj00@gmail.com',
+    url             = 'https://github.com/Honzin/ccs',
+    download_url    = 'https://github.com/Honzin/ccs/tarball/0.1',
+    keywords        = ['bitfinex', 'bitstamp', 'bittrex', "btcc", "btce", "bter", "cexio", "kraken", "okcoin", "poloniex", "api", "crypto", "coin", "stock", "market"],
+    classifiers     = [],
 )
