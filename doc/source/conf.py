6b0cdcddee68191ecec337e367e1749b69f9c8ae
Honza <xsedaj00@gmail.com>
2017-02-04 13:20:08 +0100

    Fix for readTheDocs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -159,4 +159,15 @@ texinfo_documents = [
 ]
 
 
+import sys
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['http.client', 'constants']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 
